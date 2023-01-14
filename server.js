5588063061c3059e196c3c1dde5e4ff052049df6
IronFury0 <Alexfuryjanes@gmail.com>
2023-01-14 04:11:00 -0500

    502 error checks

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ mc.connect(mongoUrl, { useNewUrlParser: true },(err, client) => {
   
     db = client.db('testing');
     
-    const port = process.env.PORT || 3000;
+    const port = process.env.port || 3000;
     app.listen(port);
     console.log("Listening on port 3000");
 });
